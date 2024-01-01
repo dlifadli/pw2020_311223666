258a46b22a47fdcb1089c2ac11a372cb4e327632
Fadli <ffadlizuhri@gmail.com>
2024-01-01 10:26:15 +0700

    menambahkan pertemuan11 & menambahkan fitur hapus & ubah

---
+++ kuliah x/pertemuan11/detail.php
@@ -0,0 +1,39 @@
+<?php
+require "functions.php";
+
+// jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+// ambil id dari URL
+$id = $_GET["id"];
+
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $m['gambar']; ?>" width="100"></li>
+    <li>NRP : <?= $m['nrp']; ?></li>
+    <li>Nama : <?= $m['nama']; ?></li>
+    <li>Email : <?= $m['email']; ?></li>
+    <li>Jurusan : <?= $m['jurusan']; ?></li>
+    <li><a href="ubah.php?id=<?= $m['id']; ?>">Ubah</a> | <a href="hapus.php?id=<?= $m['id']; ?>" onclick="return confirm('Apakah Anda Yakin?');">Hapus</a></li>
+    <li><a href="index.php">Kembali ke Daftar Mahasiswa</a></li>
+  </ul>
+
+</body>
+
+</html>
